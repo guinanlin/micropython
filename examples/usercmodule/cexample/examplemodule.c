6c60deab4fea9c9d7304c1a9d3288aad01a85695
guinan.lin <guinan.lin@foxmail.com>
2023-11-11 18:20:56 +0800

    update

--- examples/usercmodule/cexample/examplemodule.c
+++ examples/usercmodule/cexample/examplemodule.c
@@ -89,4 +89,4 @@ const mp_obj_module_t example_user_cmodule = {
 // Register the module to make it available in Python.
 // test 1 flag
 // MP_REGISTER_MODULE(MP_QSTR_cexample, example_user_cmodule);
-MP_REGISTER_MODULE(MP_QSTR_cexample, example_user_cmodule, 1);
+MP_REGISTER_MODULE(MP_QSTR_cexample, example_user_cmodule);
--- ports/esp32/boards/ESP32_GENERIC_S3/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_S3/mpconfigboard.h
@@ -1,6 +1,6 @@
 #ifndef MICROPY_HW_BOARD_NAME
 // Can be set by mpconfigboard.cmake.
-#define MICROPY_HW_BOARD_NAME               "Generic ESP32S3 module"
+#define MICROPY_HW_BOARD_NAME               "Generic DTY ESP32S3 module"
 #endif
 #define MICROPY_HW_MCU_NAME                 "ESP32S3"
 
@@ -11,3 +11,5 @@
 
 #define MICROPY_HW_I2C0_SCL                 (9)
 #define MICROPY_HW_I2C0_SDA                 (8)
+
+#define MODULE_CEXAMPLE_ENABLED             (1)
